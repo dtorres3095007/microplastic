535b8f15354349ad52449ed46b8821de8727616d
Lballest25 <87829828+Lballest25@users.noreply.github.com>
2025-12-16 12:55:52 -0500

    Update trainer_predictor.py

--- trainer_predictor.py
+++ trainer_predictor.py
@@ -48,16 +48,16 @@ def predictor_request():
         )
 
         for step in [
-            predictor.clean_folders,
+            # predictor.clean_folders,
             predictor.get_polygon_images,
-            predictor.calculate_features,
-            predictor.feature_mean,
-            predictor.bands_means,
-            predictor.create_polygons,
-            predictor.create_dataset,
-            predictor.predict,
-            predictor.show_map,
-            predictor.outputs_db,
+            # predictor.calculate_features,
+            # predictor.feature_mean,
+            # predictor.bands_means,
+            # predictor.create_polygons,
+            # predictor.create_dataset,
+            # predictor.predict,
+            # predictor.show_map,
+            # predictor.outputs_db,
         ]:
             status, message = step()
             if status != STATUS_OK:
