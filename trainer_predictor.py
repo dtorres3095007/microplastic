cfd1a2719eaa5e29bf6ecb9fd2d2025b809d7db2
Lballest25 <87829828+Lballest25@users.noreply.github.com>
2025-12-22 12:42:02 -0500

    Update trainer_predictor.py

--- trainer_predictor.py
+++ trainer_predictor.py
@@ -49,8 +49,8 @@ def predictor_request():
 
         for step in [
             # predictor.clean_folders,
-            predictor.get_polygon_images,
-            # predictor.calculate_features,
+            # predictor.get_polygon_images,
+            predictor.calculate_features,
             # predictor.feature_mean,
             # predictor.bands_means,
             # predictor.create_polygons,
