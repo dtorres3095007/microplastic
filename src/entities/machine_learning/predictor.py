4d80c3cf50749e1e9d1a67f04c83391f144a01f9
Lballest25 <87829828+Lballest25@users.noreply.github.com>
2026-02-10 13:43:27 -0500

    Update predictor.py

--- src/entities/machine_learning/predictor.py
+++ src/entities/machine_learning/predictor.py
@@ -455,7 +455,7 @@ class Predictor:
             *FOLDERS_DATASET_NAMES["MAIN"], FOLDERS_DATASET_NAMES["MODELS"]
         )
         model = Models(dataset_path, models_path)
-        print("Variable model: ", model)
+        print("model.__dict__ =", model.__dict__)
         status, message = model.load_data()
 
         if status != STATUS_OK:
